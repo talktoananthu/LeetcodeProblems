71146113fe78b788077d77db032eb4f9911ccff9
Ananthu <100486513+talktoananthu@users.noreply.github.com>
2026-05-09 22:17:42 +0530

    Time: 0 ms (100.00%) | Memory: 22.6 MB (50.86%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -14,4 +14,5 @@ public:
 
         return i + 1;
     }
+    //here actually 2 pointer approach is used elements are removed but the duplicates elements are being pushed to front then returning the last pushed elements ith index value +1
 };
